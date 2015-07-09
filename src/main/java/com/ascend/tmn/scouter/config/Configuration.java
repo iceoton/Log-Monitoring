ca1150df5a057c03eea27eabd1af5d978b0ba3c6
Thakoon Thepsiri <thakoon_the@truecorp.co.th>
2015-07-09 18:07:12 +0700

    setting table name read from external file.

--- src/main/java/com/ascend/tmn/scouter/config/Configuration.java
+++ src/main/java/com/ascend/tmn/scouter/config/Configuration.java
@@ -1,25 +1,27 @@
 package com.ascend.tmn.scouter.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * Created by keerati on 7/2/15 AD.
  */
-@PropertySource("classpath:database.properties")
 @Component
+@PropertySource("file:/data/database.properties")
 public class Configuration {
 
-
-
-    private static String tableName;
+    @Value("${mysql.tablename}")
+    private  String tableName;
 
     private long lowerRandomSleepTimeRange = 0L;
     private long upperRandomSleepTimeRange = 1000L;
 
     public Configuration() {
-        tableName = System.getenv("TABLE_NAME");
     }
 
     public long getUpperRandomSleepTimeRange() {
